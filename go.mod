5759799683136fd189f54dbfab830ddf5d13e24f
xavier2910 <x.distelzweig@gmail.com>
2024-02-21 16:21:42 -0600

    test tundra dep

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/xavier2910/tundragame
 
 go 1.21.5
+
+require github.com/xavier2910/tundra v0.1.0
--- main.go
+++ main.go
@@ -1,7 +1,15 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/xavier2910/tundra"
+)
 
 func main() {
 	fmt.Printf("The Tundra, take 2, version 0.0.0.\nNothing implemented yet\n")
+	fmt.Printf("%#v\n", tundra.NewWorld(
+		tundra.NewPlayer(),
+		[]*tundra.Location{},
+	))
 }
