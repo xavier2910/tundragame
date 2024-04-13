f4d41dca05095f20e9f6bd47973b015971e9c7ce
xavier2910 <x.distelzweig@gmail.com>
2024-04-13 18:09:46 -0500

    added much more content: most of the structure of the starting planet

--- internal/story/story.go
+++ internal/story/story.go
@@ -9,6 +9,9 @@ import (
 )
 
 var GameData *tundra.World
+var (
+	lampOn = false
+)
 
 func MustInitGameData() {
 
@@ -31,6 +34,47 @@ func MustInitGameData() {
 		},
 		Commands: map[string]tundra.Command{},
 	}
+	nearforest := &tundra.Location{
+		Title:       "North of Forest",
+		Description: "You are standing at the north edge of a dark conifer forest, which bends northward to the west and southward to the east.",
+		Objects:     map[string]*tundra.Object{},
+		Commands:    map[string]tundra.Command{},
+	}
+	overcave := &tundra.Location{
+		Title:       "Top of Cliff",
+		Description: "You are standing atop a tall icy cliff facing west. A conifer forest lies to your east, stretching away to the north and south.",
+		Objects:     map[string]*tundra.Object{},
+		Commands:    map[string]tundra.Command{},
+	}
+	westnearforest := &tundra.Location{
+		Title:       "East of Forest",
+		Description: "You are standing under the eaves of the west edge of a conifer forest. The forest edge continues off to the north and to the southeast. To the east you can see a cliff.",
+		Objects:     map[string]*tundra.Object{},
+		Commands:    map[string]tundra.Command{},
+	}
+	house := &tundra.Location{
+		Title:       "Forest House",
+		Description: "You are standing at the southern edge of a forest, which stretches away to the east and west. There is a small empty-looking house nestled under the eaves of the dark conifer forest.",
+		Objects:     map[string]*tundra.Object{},
+		Commands:    map[string]tundra.Command{},
+	}
+	fireplace := tundra.NewObject(tundra.WithDescription("The fireplace is bare and black."))
+	lamp := tundra.NewObject(tundra.WithDescription("This lamp appears to still have plenty of oil in it."))
+	inhouse := &tundra.Location{
+		Title:       "Inside House",
+		Description: "You are standing inside a small house.",
+		Objects: map[string]*tundra.Object{
+			"fireplace": fireplace,
+			"lamp":      lamp,
+		},
+		Commands: map[string]tundra.Command{},
+	}
+	inforest := &tundra.Location{
+		Title:       "In Forest",
+		Description: "The forest surrounding you is very dark",
+		Objects:     map[string]*tundra.Object{},
+		Commands:    map[string]tundra.Command{},
+	}
 
 	GameData = tundra.NewWorld(
 		tundra.NewPlayer(
@@ -41,6 +85,12 @@ func MustInitGameData() {
 		[]*tundra.Location{
 			start,
 			incave,
+			nearforest,
+			overcave,
+			westnearforest,
+			house,
+			inhouse,
+			inforest,
 		},
 	)
 
@@ -61,12 +111,51 @@ func MustInitGameData() {
 		}, nil
 
 	})
-
 	penny.AddCommand("take", take("penny", penny))
 	penny.AddCommand("drop", drop("penny", penny))
 	chair.AddCommand("examine", commands.Examine(chair))
 	chair.AddCommand("take", take("chair", chair))
 	chair.AddCommand("drop", drop("chair", chair))
+	fireplace.AddCommand("examine", commands.Examine(fireplace))
+	lamp.AddCommand("light", func(o []*tundra.Object) (tundra.CommandResults, error) {
+		if lampOn {
+			return tundra.CommandResults{
+				Result: tundra.Ok,
+				Msg:    []string{"The lamp is already on."},
+			}, nil
+		}
+		lampOn = true
+		return tundra.CommandResults{
+			Result: tundra.Ok,
+			Msg:    []string{"You turn the lamp on. It has enough oil to last for quite a while."},
+		}, nil
+	})
+	lamp.AddCommand("snuff", func(o []*tundra.Object) (tundra.CommandResults, error) {
+		if !lampOn {
+			return tundra.CommandResults{
+				Result: tundra.Ok,
+				Msg:    []string{"The lamp is already off."},
+			}, nil
+		}
+		lampOn = false
+		return tundra.CommandResults{
+			Result: tundra.Ok,
+			Msg:    []string{"You turn the lamp off. It still has enough oil to last for quite a while."},
+		}, nil
+	})
+	lamp.AddCommand("take", take("lamp", lamp))
+	lamp.AddCommand("examine", func(o []*tundra.Object) (tundra.CommandResults, error) {
+		var msg string
+		if lampOn {
+			msg = fmt.Sprintf("%s. The lamp is on.", lamp.Description)
+		} else {
+			msg = fmt.Sprintf("%s. The lamp is off.", lamp.Description)
+		}
+		return tundra.CommandResults{
+			Result: 0,
+			Msg:    []string{msg},
+		}, nil
+	})
 }
 
 func MustCreateCommands(cp tundra.CommandProcessor) {
@@ -91,8 +180,92 @@ func MustCreateCommands(cp tundra.CommandProcessor) {
 }
 
 func MustConnectLocations(cp tundra.CommandProcessor) {
+	//start to incave
 	GameData.Places[0].SetConnection(tundra.East, GameData.Places[1], GameData.PlayerData, cp)
+	GameData.Places[0].SetConnection(tundra.In, GameData.Places[1], GameData.PlayerData, cp)
+	//incave to start
 	GameData.Places[1].SetConnection(tundra.West, GameData.Places[0], GameData.PlayerData, cp)
+	GameData.Places[1].SetConnection(tundra.Out, GameData.Places[0], GameData.PlayerData, cp)
+	//nearforest to start
+	GameData.Places[2].SetConnection(tundra.North, GameData.Places[0], GameData.PlayerData, cp)
+	//start to nearforest
+	GameData.Places[0].SetConnection(tundra.South, GameData.Places[2], GameData.PlayerData, cp)
+	//overcave to nearforest
+	GameData.Places[3].SetConnection(tundra.Southwest, GameData.Places[2], GameData.PlayerData, cp)
+	GameData.Places[3].SetConnection(tundra.South, GameData.Places[2], GameData.PlayerData, cp)
+	//nearforest to overcave
+	GameData.Places[2].SetConnection(tundra.Northeast, GameData.Places[3], GameData.PlayerData, cp)
+	GameData.Places[2].SetConnection(tundra.East, GameData.Places[3], GameData.PlayerData, cp)
+	//start to westnearforest
+	GameData.Places[0].SetConnection(tundra.West, GameData.Places[4], GameData.PlayerData, cp)
+	//wesstnearforest to start
+	GameData.Places[4].SetConnection(tundra.East, GameData.Places[0], GameData.PlayerData, cp)
+	//westnearforest to nearforest
+	GameData.Places[4].SetConnection(tundra.Southeast, GameData.Places[2], GameData.PlayerData, cp)
+	GameData.Places[4].SetConnection(tundra.South, GameData.Places[2], GameData.PlayerData, cp)
+	//start to house
+	GameData.Places[0].SetConnection(tundra.North, GameData.Places[5], GameData.PlayerData, cp)
+	//house to start
+	GameData.Places[5].SetConnection(tundra.South, GameData.Places[0], GameData.PlayerData, cp)
+	//house to overcave
+	GameData.Places[5].SetConnection(tundra.Southwest, GameData.Places[3], GameData.PlayerData, cp)
+	GameData.Places[5].SetConnection(tundra.West, GameData.Places[3], GameData.PlayerData, cp)
+	//house to westnearforest
+	GameData.Places[5].SetConnection(tundra.Southeast, GameData.Places[4], GameData.PlayerData, cp)
+	GameData.Places[5].SetConnection(tundra.East, GameData.Places[4], GameData.PlayerData, cp)
+	//westnearforest to house
+	GameData.Places[4].SetConnection(tundra.North, GameData.Places[5], GameData.PlayerData, cp)
+	GameData.Places[4].SetConnection(tundra.Northeast, GameData.Places[5], GameData.PlayerData, cp)
+	//house to inhouse
+	GameData.Places[5].SetConnection(tundra.In, GameData.Places[6], GameData.PlayerData, cp)
+	//inhouse to house
+	GameData.Places[6].SetConnection(tundra.Out, GameData.Places[5], GameData.PlayerData, cp)
+	//house to inforest
+	GameData.Places[5].SetConnection(tundra.North, GameData.Places[7], GameData.PlayerData, cp)
+	GameData.Places[5].SetConnection(tundra.Northwest, GameData.Places[7], GameData.PlayerData, cp)
+	GameData.Places[5].SetConnection(tundra.Northeast, GameData.Places[7], GameData.PlayerData, cp)
+	//nearforest to inforest
+	GameData.Places[2].SetConnection(tundra.South, GameData.Places[7], GameData.PlayerData, cp)
+	GameData.Places[2].SetConnection(tundra.Southwest, GameData.Places[7], GameData.PlayerData, cp)
+	GameData.Places[2].SetConnection(tundra.Southeast, GameData.Places[7], GameData.PlayerData, cp)
+	//westnearforest to inforest
+	GameData.Places[4].SetConnection(tundra.West, GameData.Places[7], GameData.PlayerData, cp)
+	GameData.Places[4].SetConnection(tundra.Southwest, GameData.Places[7], GameData.PlayerData, cp)
+	GameData.Places[4].SetConnection(tundra.Northwest, GameData.Places[7], GameData.PlayerData, cp)
+	//overcave to inforest
+	GameData.Places[3].SetConnection(tundra.East, GameData.Places[7], GameData.PlayerData, cp)
+	GameData.Places[3].SetConnection(tundra.Northeast, GameData.Places[7], GameData.PlayerData, cp)
+	GameData.Places[3].SetConnection(tundra.Southeast, GameData.Places[7], GameData.PlayerData, cp)
+
+	// now we allow the player to go various places from in the forest if they have the lamp and it's on
+	// but if they don't have the lamp or it's off, they get eaten by a wild beast.
+	hookUpinforestTo := func(direction tundra.Direction, other *tundra.Location) {
+		conn := tundra.NewObject()
+		conn.AddCommand("go", func(o []*tundra.Object) (tundra.CommandResults, error) {
+			if lampOn && GameData.PlayerData.Inventory["lamp"] != nil {
+				GameData.PlayerData.CurLoc = other
+				cp.UpdateContext()
+				return tundra.CommandResults{
+					Result: tundra.Ok,
+					Msg:    []string{"# " + other.Title + "\n\n" + other.Description},
+				}, nil
+			} else {
+				return tundra.CommandResults{
+					Result: tundra.Death,
+					Msg:    []string{"You blunder around in the dark woods and are eaten by a wild beast."},
+				}, nil
+			}
+		})
+		GameData.Places[7]. /*inforest*/ Objects[string(direction)] = conn
+	}
+	//to nearforest
+	hookUpinforestTo(tundra.North, GameData.Places[2])
+	//to westnearforest
+	hookUpinforestTo(tundra.East, GameData.Places[4])
+	//to overcave
+	hookUpinforestTo(tundra.West, GameData.Places[3])
+	//to house
+	hookUpinforestTo(tundra.South, GameData.Places[5])
 }
 
 func take(name string, obj *tundra.Object) tundra.Command {
